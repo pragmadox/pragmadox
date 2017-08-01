7b423efc93da731cc05900458785d28fbcae6759
Jay Price <jxprice@live.com>
2017-08-01 16:06:18 -0400

    I made some changes and things like that.

--- contactProc.php
+++ contactProc.php
@@ -9,16 +9,18 @@
             $name= $_POST['name'];
             $email = $_POST['email'];
             $message = $_POST['message'];
+            $company = $_POST['company'];
             $requestType = $_POST['requestType'];
             $msg = "Message from a Potential Customer: \n\n";
             $msg .= "Name: " . $name . "\n";
             $msg .= "Email: " . $email . "\n";
+            $msg .= "Company: " . $company . "\n";
             $msg .= "Request Type: " . $requestType . "\n";
-            $msg .= "Message: " . $message . "\n";
+            $msg .= "Message: \n\n" . $message . " ";
     
             //mail vars
-            $to = "jay.price@pragmadox.com, pgrimes1991@gmail.com";
-            $subject = " " . $requestType . " from " . $firstName . " " . $lastName . " ";
+            $to = "jay.price@pragmadox.com";
+            $subject = " " . $requestType . " from " . $name . " ";
             $mailheaders = "From: " . $email . "\n";
             $mailheaders .= "Reply-to: " . $email . "\n";
             //send the email!!
@@ -27,6 +29,7 @@
         ?>
         <script>
             window.location.replace("index.html");
+            alert("Thank you so much for your message. I will contact you shortly.");
         </script>
     </head>
 <body>
